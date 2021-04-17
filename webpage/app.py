51bfd4c637acafb482ec771982185ec5d51986f9
jchoi17 <joan.choi@ucla.edu>
2021-04-17 11:30:05 -0700

    push

--- webpage/app.py
+++ webpage/app.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import sqlite3
 from sqlalchemy.orm import Session
-  
-from flask import Flask, request, render_template, jsonify
+import pprint
+from flask import Flask, request, render_template, json, current_app
 
 #################################################
 # Flask Setup
@@ -19,33 +19,18 @@ def welcome():
     """List all available api routes."""
     return render_template('index.html')
 
-    #################################################
-    # Database Setup
-    #################################################
-    con = sqlite3.connect("../data/project2.sqlite")
-    test_run = pd.read_sql_query("SELECT * from WorldHappiness", con)
-
-    print(test_run)
-
-    # Convert list of tuples into normal list
-    all_names = test_run.to_json()
-
-    return jsonify(all_names)
-
 @app.route("/landing")
 def names():
     #################################################
     # Database Setup
     #################################################
     con = sqlite3.connect("../data/project2.sqlite")
-    test_run = pd.read_sql_query("SELECT * from WorldHappiness", con)
-
-    print(test_run)
+    test_run = pd.read_sql_query("SELECT * from df", con)
 
     # Convert list of tuples into normal list
-    all_names = test_run.to_json()
+    data = test_run.to_json()
 
-    return jsonify(all_names)
+    return current_app.response_class(json.dumps(data))
 
 if __name__ == '__main__':
     app.run(debug=True)
