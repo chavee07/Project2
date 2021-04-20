0028e6f09ec301fc9d644453debfe6947515ca74
jchoi17 <joan.choi@ucla.edu>
2021-04-19 22:09:07 -0700

    updated flask

--- webpage/app.py
+++ webpage/app.py
@@ -3,6 +3,7 @@ import sqlite3
 from sqlalchemy.orm import Session
 import pprint
 from flask import Flask, request, render_template, jsonify, current_app
+import json
 
 #################################################
 # Flask Setup
@@ -27,19 +28,49 @@ def home():
 
 @app.route("/landing")
 def names():
+
     #################################################
     # Database Setup
     #################################################
     con = sqlite3.connect("../data/project2.sqlite")
-    test_run = pd.read_sql_query("SELECT * from df", con)
+    db = con.cursor()
+
+    # country = db.execute('SELECT Country from df').fetchall()
+    # con_code = db.execute('SELECT "Country Code" from df').fetchall()
+    # score = db.execute('SELECT Score from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
+    # country = db.execute('SELECT Country from df').fetchall()
 
     # Convert list of tuples into normal list
-    data = test_run.to_json()
+    results = db.execute('Select * from df').fetchall()
 
-    # return current_app.response_class(json.dumps(data))
+    # return jsonify(data)
 
-    return jsonify(data)
 
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_countries = []
+    for country, con_code, score, gdp, social, health, freedom, generosity, corruption, alcohol in results:
+        countries_dict = {}
+        countries_dict["Country"] = country
+        countries_dict["Country Code"] = con_code
+        countries_dict["Score"] = score
+        countries_dict["GDP per capita"] = gdp
+        countries_dict["Social support"] = social
+        countries_dict["Health life expectancy"] = health
+        countries_dict["Freedom to make life choices"] = freedom
+        countries_dict["Generosity"] = generosity
+        countries_dict["Perceptions of corruption"] = corruption
+        countries_dict["Alcohol Consumption per Capita (liter)"] = alcohol
+        all_countries.append(countries_dict)
+
+    return jsonify(all_countries)
+    # # return current_app.response_class(json.dumps(data))
+    
 @app.route("/maps")
 def maps():
     return render_template('maps.html')
