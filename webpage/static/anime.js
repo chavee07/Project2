da1cbdd53e2200ab84b10a5da2fd5704d3081b83
jchoi17 <joan.choi@ucla.edu>
2021-04-19 20:24:58 -0700

    updates

--- webpage/static/anime.js
+++ webpage/static/anime.js
@@ -1,11 +1,9 @@
-JAVASCRIPT
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml2');
+var textWrapper = document.getElementById("letters");
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
 anime.timeline({loop: true})
   .add({
-    targets: '.ml2 .letter',
+    targets: '.banner-area .letter',
     scale: [4,1],
     opacity: [0,1],
     translateZ: 0,
@@ -13,9 +11,10 @@ anime.timeline({loop: true})
     duration: 950,
     delay: (el, i) => 70*i
   }).add({
-    targets: '.ml2',
-    opacity: 0,
+    targets: '.banner-area .letter',
+    opacity: .2,
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
+
   });
